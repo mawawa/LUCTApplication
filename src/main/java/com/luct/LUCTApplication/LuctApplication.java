d917e43ed101c1f5478fe9bed25acdd4d3a70b06
wandile <nanashongwe@gmail.com>
2025-04-28 14:57:37 +0200

    Initial commit

---
+++ src/main/java/com/luct/LUCTApplication/LuctApplication.java
@@ -0,0 +1,13 @@
+package com.luct.LUCTApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luct/LUCTApplication/LuctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luct.LUCTApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
